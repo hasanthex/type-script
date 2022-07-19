50626c9defb33c10fb0f79f49c8f7fe7a804e945
hasanthex <hasan18353@gmail.com>
2022-07-19 15:55:58 +0600

    First Commit

---
+++ basic/project-setup/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log('Running... ... ...');
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply('10', 10));
+let country = ["Bangladesh", "India", 1];
+country.push("Australia");
+country.push(30);
+console.log(country);
---
+++ basic/project-setup/src/script.ts
@@ -0,0 +1,13 @@
+console.log('Running... ... ...');
+
+function multiply(a : number, b : number){
+  return a*b
+}
+console.log(multiply('10',10))
+
+
+
+let country = ["Bangladesh", "India", 1];
+country.push("Australia")
+country.push(30)
+console.log(country)
